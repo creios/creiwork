0eecdb2489926ebe2e4e6c6ffe56d1bd35445037
Tim Tegeler <tim@tegeler.me>
2017-01-17 20:50:40 +0100

    Using new Creiwork class

--- public/index.php
+++ public/index.php
@@ -1,32 +1,12 @@
 <?php
 
-use DI\ContainerBuilder;
-use Noodlehaus\Config;
-use TimTegeler\Routerunner\Routerunner;
-use Whoops\Handler\PrettyPageHandler;
-use function Creios\Creiwork\Framework\out;
+use Creios\Creiwork\Framework\Creiwork;
 
-date_default_timezone_set('UTC');
-session_start();
-ob_start();
-
+// let composer load everything
 require_once "../vendor/autoload.php";
 
-$containerBuilder = new ContainerBuilder();
-$containerBuilder->addDefinitions('../config/di-config.php');
-$container = $containerBuilder->build();
+// set path to config file
+$config = __DIR__ . '/../config/config.json';
 
-$config = $container->get(Config::class);
-
-if($config->get('debug')){
-    $whoops = $container->get(\Whoops\Run::class);
-    $whoops->pushHandler($container->get(PrettyPageHandler::class));
-    $whoops->register();
-}
-
-$routerunner = $container->get(Routerunner::class);
-/** @var GuzzleHttp\Psr7\Response $response */
-$response = $routerunner->execute($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
-
-ob_end_clean();
-out($response);
+// create a new Creiwork instance and start execution
+(new Creiwork($config))->start();

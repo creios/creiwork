d134d362af8e7fe120e16276fd1975eb30260d4b
Jonas Schürmann <schuermann@creios.net>
2018-06-13 16:39:12 +0200

    Fixes for version 3

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Creios\Creiwork\Framework\Creiwork;
 require_once "../vendor/autoload.php";
 
 // set path to config file
-$config = __DIR__ . '/../config/config.json';
+$config = __DIR__ . '/../config';
 
 // create a new Creiwork instance and start execution
 (new Creiwork($config))->start();

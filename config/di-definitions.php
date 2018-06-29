b172eb31f9342b93cbaa848654ac4609b811bbb8
Benjamin Knauer <ben.knauer@gmail.com>
2018-06-29 10:31:33 +0200

    di-definitions.php is now empty by default

--- config/di-definitions.php
+++ config/di-definitions.php
@@ -1,28 +1,7 @@
 <?php
 
-use League\Plates;
-use Quarry\Framework\Database;
-use Quarry\Framework\PdoDatabase;
-use function DI\autowire;
-use function DI\create;
-
+// array of custom definitions
+// https://github.com/PHP-DI/PHP-DI/blob/4.x/doc/definition.md
 return [
 
-    \PDO::class => function (Config $config) {
-        $host = $config->get('database.host');
-        $database = $config->get('database.database');
-        $user = $config->get('database.user');
-        $password = $config->get('database.password');
-
-        $dsn = "mysql:dbname={$database};host={$host};charset=UTF8";
-        $pdo = new \PDO($dsn, $user, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        return $pdo;
-    },
-    Database::class =>
-        autowire(PdoDatabase::class),
-
-    Plates\Engine::class =>
-        create()->constructor(__DIR__ . '/../templates'),
 ];

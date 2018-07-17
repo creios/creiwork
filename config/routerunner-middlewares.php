ec0ca81c88506ad7e0fa1947d5f24c16dfc5edc9
Benjamin Knauer <knauer@creios.net>
2018-07-17 09:32:55 +0200

    filled routerunner-middlewares.php with dummy content

---
+++ config/routerunner-middlewares.php
@@ -0,0 +1,13 @@
+<?php
+
+// array of routerunner middleware objects
+// https://github.com/timtegeler/routerunner
+
+use TimTegeler\Routerunner\Components\Call;
+// use Creios\Creiwork\Middleware\Routerunner\MyRouterunnerMiddleware;
+// use Creios\Creiwork\Controller\MyController;
+
+
+return [
+    //new MyRouterunnerMiddleware(new Call(MyController::class, 'myMethod'))
+];

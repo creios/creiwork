81ac89cbec99d73dfdf21bbe9a72484fbc325889
Benjamin Knauer <knauer@creios.net>
2018-07-18 16:54:54 +0200

    added config for xhgui

--- public/index.php
+++ public/index.php
@@ -3,6 +3,13 @@
 use Creios\Creiwork\Framework\Creiwork;
 use Middlewares\TrailingSlash;
 
+// Uncomment this to use profiling via xhgui
+// xhgui should be preconfigured in context of docker
+
+// if (file_exists('/xhgui-service/external/header.php')) {
+//     require_once '/xhgui-service/external/header.php';
+// }
+
 // let composer load everything
 require_once "../vendor/autoload.php";
 
